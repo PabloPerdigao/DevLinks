9b6d5ca512e013318d4f9ddcb052e97228b5960a
Pablo Perdigão <pabloagps@gmail.com>
2024-03-03 15:29:49 -0300

    Update

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-function toggleMode() {
-  const html = document.documentElement;
-  html.classList.toggle("light");
+// Obtém o modo atual do localStorage ou define como 'dark' se não existir
+const currentMode = localStorage.getItem('mode') || 'dark';
+const html = document.documentElement;
 
-  // Lista de ícones e seus caminhos para light mode
+// Aplica o modo atual ao carregar a página
+html.classList.add(currentMode);
+
+// Atualiza os ícones de acordo com o modo
+updateIcons();
+
+// Função para alternar entre os modos
+function toggleMode() {
+  html.classList.toggle("light");
+  const newMode = html.classList.contains("light") ? "light" : "dark";
+  
+  // Salva o modo atual no localStorage
+  localStorage.setItem('mode', newMode);
+
+  // Atualiza os ícones após a alteração do modo
+  updateIcons();
+}
+
+// Função para atualizar os ícones com base no modo
+function updateIcons() {
   const icons = {
-    github: "./mobile/light-mode/github.png",
-    instagram: "./mobile/light-mode/instagram.png",
-    linkedin: "./mobile/light-mode/linkedin.png"
+    github: "./mobile/GitHub.png",
+    instagram: "./mobile/Instagram.png",
+    linkedin: "./mobile/LinkedIn.png"
   };
 
   // Iterar sobre cada ícone e substituir a imagem conforme necessário
   for (const [iconId, iconPath] of Object.entries(icons)) {
-    const iconElement = document.querySelector(`#social-links a[href*="${iconId}"] img`);
-
-    if (html.classList.contains("light")) {
-      iconElement.src = iconPath;
-    } else {
-      // Se não estiver no light mode, usar o caminho normal
-      iconElement.src = `./mobile/${iconId}.png`;
-    }
+    const img = document.querySelector(`#social-links a[href*="${iconId}"] img`);
+    const modePath = `./mobile/${html.classList.contains("light") ? 'light-mode/' : ''}${iconId}.png`;
+    img.setAttribute("src", modePath);
   }
 }
