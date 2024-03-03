b636c6535c31081e5abd8b51f0efd151733d30f4
Pablo Perdigão <pabloagps@gmail.com>
2024-03-03 13:12:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// Obtém o modo atual do localStorage ou define como 'light' se não existir
-const currentMode = localStorage.getItem('mode') || 'light';
-const html = document.documentElement;
-
-// Aplica o modo atual ao carregar a página
-html.classList.add(currentMode);
-
-// Função para trocar o modo light/dark
 function toggleMode() {
-  // Inverte a classe light no elemento HTML
+  const html = document.documentElement;
   html.classList.toggle("light");
 
   // Lista de ícones e seus caminhos para light mode
@@ -19,21 +11,13 @@ function toggleMode() {
 
   // Iterar sobre cada ícone e substituir a imagem conforme necessário
   for (const [iconId, iconPath] of Object.entries(icons)) {
-    const img = document.querySelector(`#social-links a[href*="${iconId}"] img`);
+    const iconElement = document.querySelector(`#social-links a[href*="${iconId}"] img`);
 
     if (html.classList.contains("light")) {
-      img.setAttribute("src", iconPath);
+      iconElement.src = iconPath;
     } else {
       // Se não estiver no light mode, usar o caminho normal
-      img.setAttribute("src", `./mobile/${iconId}.png`);
+      iconElement.src = `./mobile/${iconId}.png`;
     }
   }
-
-  // Atualiza o localStorage com o modo atual
-  const updatedMode = html.classList.contains("light") ? 'light' : 'dark';
-  localStorage.setItem('mode', updatedMode);
 }
-
-// Chama a função toggleMode() ao carregar a página
-toggleMode();
-
